b72ebcce651c72b600a7a6096b5d5da5643073fa
LSAB <lsabkwaku@gmail.com>
2025-08-05 17:22:34 -0500

    added the ability check all python files in a folder

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
 import os
 import sys
 import subprocess
+import time
+from pathlib import Path
+
 
 file = sys.argv[1]
 
-
-
+use_folder = False
+if len(sys.argv) >=3:
+    if sys.argv[2] == 'f':
+        use_folder = True
 path = os.getcwd()
 
 running = False
+prompt = 'New Process Started'
+
+accepted_ext = ['*.py']
+
+def get_change_time():
+    if use_folder:
+        lst = []
+        for ext in accepted_ext:
+            lst.extend(Path(path).glob(ext))
+        t = 0
+        for _ in lst:
+            t+=os.path.getmtime(_)
+
+        return t
+    else:
+        return os.path.getmtime(file)
+
+
+
+
 while True:
     try:
         if path and not running:
             os.chdir(path)
-            last_change = os.path.getmtime(file)
+            last_change = get_change_time()
+            print(f'{prompt:<10}')
             run = subprocess.Popen(str(f'py {file}').split())
-        current_change = os.path.getmtime(file)
+        current_change = get_change_time()
         running = True
-        
         
         if last_change < current_change:
             run.terminate()
             run.kill()
-            prompt = 'New Process Started'
-            print(f'{prompt:>10}')
+            print(f'{prompt:<10}')
             
             run = subprocess.Popen(str(f'py {file}').split())
             last_change = current_change
+        time.sleep(0.3)
     except KeyboardInterrupt:
         run.terminate()
         run.kill()
